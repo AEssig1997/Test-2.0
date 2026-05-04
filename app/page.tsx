390f78244823be1b0920f7c298bfb780eb0d688c
AEssig1997 <alexander.m.essig@gmail.com>
2026-05-03 23:19:35 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Aelana Home</h1>
+      <p>Select a page from the sidebar</p>
+    </div>
+  );
+}
